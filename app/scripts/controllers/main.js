c242ba95607f2f9af7e03e99a006934e5eef07d9
Benedek Gagyi <gagyibenedek@yahoo.com>
2015-01-20 19:13:21 +0100

    base working version

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,12 +1,37 @@
 angular.module('trello', ['firebase']);
 
+
 angular.module('trello').controller('MainController', ['$scope', '$firebase',
   function ($scope, $firebase) {
     var ref = new Firebase("https://amber-heat-6761.firebaseio.com/");
     var sync = $firebase(ref);
 
-    $scope.messages = sync.$asArray();
+    $scope.cards = sync.$asArray();
     $scope.addMessage = function(text) {
-      $scope.messages.$add({text: text});
+      $scope.cards.$add({text: text});
+    }
+
+    $scope.editCard = function(card){
+      $scope.cards.$save(card);
+    }
+
+    $scope.deleteCard = function(card){
+      $scope.cards.$remove(card);
     }
 }]);
+
+angular.module('trello').directive('card', function(){
+  return {
+    restrict: 'A',
+    require: ['^ngModel'],
+    scope: {
+      ngModel: '=',
+      delete: '&',
+      edit: '&'
+    },
+    templateUrl: 'app/scripts/card/card.html',
+    link: function(scope, ele, attr){
+      console.log(scope.ngModel.text);
+    }
+  }
+});
